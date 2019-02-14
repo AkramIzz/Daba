3d9f3b7da6ae2cb38bec1e23ba3e4fae4f6c4f6e
Akram Izzeldin <akram.izz.kh@gmail.com>
2019-02-14 19:57:08 +0200

    Refactored logical layer impl.
    Seperated logical-physical communication from logical operations.
    Renamed StringRef to ValueRef.

--- daba/binary_tree.py
+++ daba/binary_tree.py
@@ -1,15 +1,6 @@
+from .logical import ValueRef, LogicalBase
+
 import pickle
-
-class StringRef:
-   def __init__(self, value=None, address=None):
-      self.value = value
-      self.address = address
-
-   def load(self, bytes):
-      self.value = pickle.loads(bytes)
-   
-   def serialize(self):
-      return pickle.dumps(self.value)
 
 class Node:
    def __init__(self, key, value_ref, left_ref, right_ref):
@@ -18,11 +9,7 @@ class Node:
       self.left_ref = left_ref
       self.right_ref = right_ref
 
-class NodeRef:
-   def __init__(self, value=None, address=None):
-      self.value = value
-      self.address = address
-   
+class NodeRef(ValueRef):
    def load(self, bytes):
       key, value_addr, left_addr, right_addr = pickle.loads(bytes)
  
@@ -34,10 +21,7 @@ class NodeRef:
          right_ref = NodeRef(address=right_addr)
 
       self.value = Node(
-         key,
-         StringRef(address=value_addr),
-         left_ref,
-         right_ref
+         key, ValueRef(address=value_addr), left_ref, right_ref
       )
    
    def serialize(self):
@@ -48,22 +32,12 @@ class NodeRef:
       if self.value.right_ref is not None:
          right_addr = self.value.right_ref.address
          
-      return pickle.dumps((
-         self.value.key,
-         self.value.value_ref.address,
-         left_addr,
-         right_addr
-      ))
+      return pickle.dumps(
+         (self.value.key, self.value.value_ref.address, left_addr, right_addr)
+      )
 
-class BinaryTree:
-   def __init__(self, storage):
-      self._storage = storage
-      self.root_ref = None
-
-   def set(self, key, value):
-      self._retrieve_root()
-      self.root_ref = self._set(self.root_ref, key, StringRef(value=value))
-      self.commit()
+class BinaryTree(LogicalBase):
+   RootRefClass = NodeRef
 
    def _set(self, node_ref, key, value_ref):
       new_node = None
@@ -81,10 +55,6 @@ class BinaryTree:
             node_ref.value.right_ref)
       return NodeRef(value=new_node)
 
-   def get(self, key):
-      self._retrieve_root()
-      return self._retrieve(self._get(self.root_ref, key)).value
-
    def _get(self, node_ref, key):
       if node_ref is None:
          return None
@@ -96,29 +66,6 @@ class BinaryTree:
       else:
          return self._get(self._retrieve(node_ref.value.left_ref), key)
 
-   def _retrieve_root(self):
-      if self.root_ref is not None:
-         return
-      address = self._storage.read_root_address()
-      if address is None:
-         self.root_ref = None
-         return
-      self.root_ref = NodeRef(address=address)
-      self._retrieve(self.root_ref)
-
-   def _retrieve(self, value_ref):
-      if value_ref is None or value_ref.value is not None:
-         return value_ref
-      bytes = self._storage.read(value_ref.address)
-      value_ref.load(bytes)
-      return value_ref
-
-   def commit(self):
-      if self.root_ref is None or self.root_ref.address is not None:
-         return
-      self._prepare_to_store(self.root_ref)
-      self._storage.write_root_address(self.root_ref.address)
-
    def _prepare_to_store(self, node_ref):
       if node_ref is None or node_ref.address is not None:
          return
@@ -129,13 +76,6 @@ class BinaryTree:
       self._store_value_ref(node_ref.value.value_ref)
       self._store_value_ref(node_ref)
       
-   def _store_value_ref(self, value_ref):
-      if value_ref.address is not None:
-         return
-
-      address = self._storage.write(value_ref.serialize())
-      value_ref.address = address
-
    def print(self):
       self.print_node(self.root_ref)
 
--- daba/logical.py
+++ daba/logical.py
@@ -1,3 +1,56 @@
+import pickle
+
+class ValueRef:
+   def __init__(self, value=None, address=None):
+      self.value = value
+      self.address = address
+
+   def load(self, bytes):
+      self.value = pickle.loads(bytes)
+   
+   def serialize(self):
+      return pickle.dumps(self.value)
+
 class LogicalBase:
    def __init__(self, storage):
+      self.root_ref = None
       self._storage = storage
+   
+   def set(self, key, value):
+      self._retrieve_root()
+      self.root_ref = self._set(self.root_ref, key, ValueRef(value=value))
+      self.commit()
+
+   def get(self, key):
+      self._retrieve_root()
+      return self._retrieve(self._get(self.root_ref, key)).value
+
+   def commit(self):
+      if self.root_ref is None or self.root_ref.address is not None:
+         return
+      self._prepare_to_store(self.root_ref)
+      self._storage.write_root_address(self.root_ref.address)
+
+   def _retrieve_root(self):
+      if self.root_ref is not None:
+         return
+      address = self._storage.read_root_address()
+      if address is None:
+         self.root_ref = None
+         return
+      self.root_ref = self.RootRefClass(address=address)
+      self._retrieve(self.root_ref)
+
+   def _retrieve(self, value_ref):
+      if value_ref is None or value_ref.value is not None:
+         return value_ref
+      bytes = self._storage.read(value_ref.address)
+      value_ref.load(bytes)
+      return value_ref
+
+   def _store_value_ref(self, value_ref):
+      if value_ref.address is not None:
+         return
+
+      address = self._storage.write(value_ref.serialize())
+      value_ref.address = address
